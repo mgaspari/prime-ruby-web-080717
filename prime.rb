800c6dfe626ebbca28db8991aa46109bc7ebdf1e
mgaspari <marcoanthonygaspari@gmail.com>
2017-07-26 12:44:23 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  prime = false
+  if number > 0
+  for i in 2..(number - 1)
+    if number % i == 0
+      prime = false
+      break
+    else
+      prime = true
+    end
+  end
+
+end
+  return prime
+end
